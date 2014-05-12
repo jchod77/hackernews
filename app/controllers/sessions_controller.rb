310283426d44dfc784842e496228bf983eb8e6d7
Apprentice <apprentice@dbc-chicago-004.local>
2014-05-12 16:40:07 -0500

    working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,27 @@
 class SessionsController < ApplicationController
   def new
-    @user = User.new
   end
 
   def create
     if User.authenticate(user_params)
       @user = User.find_by_username(params[:username])
       session[:id] = @user.id
+      puts "logged in******************************************************************************"
       redirect_to root_path
     else
+      puts "******************************************************************************"
       render 'new'
     end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:session).permit(params[:username], params[:password])
   end
 end
